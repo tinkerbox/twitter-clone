6a71bdae1e9048a6d34fa88ac7865986d5b2216b
Tasya Aditya Rukmana <tadityar@Tasyas-MacBook-Pro.local>
2016-05-10 09:50:27 +0800

    Added user creation (Sign Up) feature using sorcery.

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome!'
+      redirect_to new_user_path
+    else
+    	flash[:error] = 'Your registration was unsuccessful!'
+      render 'new'
+    end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :birthdate)
+	end
+end
