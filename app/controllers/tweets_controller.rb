f6c293980f5cc41d758e9eb46992c8afbfe06330
SS <shuansiang@hotmail.com>
2016-05-10 17:18:54 +0800

    Add new (empty), create and show actions to TweetsController

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,23 @@
 class TweetsController < ApplicationController
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def new
+    
+  end
+
+  def create
+    # @tweet = Tweet.new(tweet_params)
+    # @user = User.find(current_user)
+    @tweet = current_user.tweets.create(tweet_params)
+
+    # @tweet.save
+    redirect_to @tweet
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:text)
+    end
 end
