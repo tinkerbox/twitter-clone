18e40c04357afb3cde68c351bf7f4f9e7307ad8d
Tasya Aditya Rukmana <tadityar@Tasyas-MacBook-Pro.local>
2016-05-11 16:42:16 +0800

    Beautified the home page and added pagination

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
 	def show
+		@user = current_user
 		@followees = current_user.followees
-		@tweets = Tweet.all.where(user_id: @followees.ids).order('created_at DESC')
+		@tweets = Tweet.all.where(user_id: @followees.ids).order('created_at DESC').paginate(page: params[:page], per_page: 20)
 	end
 end
