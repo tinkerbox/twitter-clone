38d8e21aab79abde84c797deab3d3c94195921ec
Tasya Aditya Rukmana <tadityar@Tasyas-MacBook-Pro.local>
2016-05-11 14:48:40 +0800

    Added unfollow capability

--- app/controllers/my/follows_controller.rb
+++ app/controllers/my/follows_controller.rb
@@ -9,6 +9,10 @@ class My::FollowsController < ApplicationController
 	end
 
 	def destroy
+		@user = current_user.id
+		@other_user = params[:format]
+		follow = Follow.where(follower_id: @user, followee_id: @other_user)
+		follow.destroy
 	end
 
 end
