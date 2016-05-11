37e1ecb4ebf8bc853ad1efc644cfbf2edb84bca3
Tasya Aditya Rukmana <tadityar@Tasyas-MacBook-Pro.local>
2016-05-11 14:27:01 +0800

    Added following capabilities, added user tweets, followers, and followee info on their profiles

--- app/controllers/my/follows_controller.rb
+++ app/controllers/my/follows_controller.rb
@@ -1,2 +1,14 @@
 class My::FollowsController < ApplicationController
+	def create
+		@user = current_user.id
+		@other_user = params[:format]
+		if Follow.create(follower_id: @user, followee_id: @other_user)
+			flash[:success] = "You've successfully followed this user"
+			redirect_to user_path(params[:format])
+		end
+	end
+
+	def destroy
+	end
+
 end
