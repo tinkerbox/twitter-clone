62be0f3fc195e9bd8f30eb362cca2756d9bac8af
Tasya Aditya Rukmana <tadityar@Tasyas-MacBook-Pro.local>
2016-05-12 11:18:42 +0800

    Fixed faulty email regex, to verify users email might as well do a confirmation email later

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   validates :password, length: { minimum: 3}, confirmation: true
-  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  EMAIL_REGEX = /@/
   validates :email, uniqueness: true, presence: true, format: { with: EMAIL_REGEX , multiline: true }
   validates :username, uniqueness: true, presence: true
   validates :name, presence: true
