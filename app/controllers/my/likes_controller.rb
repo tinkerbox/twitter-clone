6f6a8c1cee53b6208514f3eff321992986f82bd4
SS <shuansiang@hotmail.com>
2016-05-12 17:55:12 +0800

    Added index page for a user's likes

--- app/controllers/my/likes_controller.rb
+++ app/controllers/my/likes_controller.rb
@@ -2,11 +2,12 @@ class My::LikesController < My::BaseController
 
   def create
     @like = current_user.likes.create(user_id: current_user.id, tweet_id: params[:tweet_id])
-
+    redirect_to :back
   end
 
   def destroy
     Like.where(user_id: current_user.id, tweet_id: params[:tweet_id]).destroy_all
+    redirect_to :back
   end
 
 end
