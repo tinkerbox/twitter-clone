42daf1dc9dfaefa7abcb08be8aa9607d8e5ce09e
Tasya Aditya Rukmana <tadityar@Tasyas-MacBook-Pro.local>
2016-05-13 10:09:52 +0800

    Added factory girl gem

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'awesome_rails_console'
   gem 'byebug'
+  gem "factory_girl_rails"
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,8 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   end
 
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
