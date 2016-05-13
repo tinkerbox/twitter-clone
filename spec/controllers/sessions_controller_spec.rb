8b9872abbc3a057689a19e43f6b220b032b4e7e5
SS <shuansiang@hotmail.com>
2016-05-13 18:16:38 +0800

    Write test for POST #create and modified GET #new of SessionController spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -6,39 +6,52 @@ describe SessionsController do
       before :example do
         @user = FactoryGirl.build(:user)
         login_user
+        get :new
       end
 
       it "redirects to the home page" do
-        get :new
-
         expect(response).to redirect_to my_home_path
       end
     end
 
     context "not logged in" do
+      before { get :new }
+
       it "responds successfully" do
-        get :new
-        
         expect(response).to be_success
         expect(response).to have_http_status(200)
       end
       
       it "renders the :new template" do
-        get :new
-        
         expect(response).to render_template("new")
       end
     end
   end
 
   describe "POST #create" do
+    before { post :create, login_params }
+
+    let(:user_password) { "somepassword" }
+    let(:user) { FactoryGirl.create(:user, password: user_password, password_confirmation: user_password ) }
+
     context "with valid account details" do
-      it "logs in the user"
-      it "redirects to the home page"
+      let(:login_params) { { email: user.email, password: user_password} }
+
+      it "logs in the user" do
+        expect(logged_in?).to eq true
+      end
+
+      it "redirects to the home page" do
+        expect(response).to redirect_to my_home_path
+      end
     end
 
     context "with invalid account details" do
-      it "renders the :new template"
+      let(:login_params) { { email: user.email, password: user_password + "asdfasd"} }
+
+      it "renders the :new template" do
+        expect(response).to render_template("new")
+      end
     end
   end
 
