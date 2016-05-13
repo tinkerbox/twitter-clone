d65104d918b10f6237d1b30775bc7bda4eb92caa
Tasya Aditya Rukmana <tadityar@Tasyas-MacBook-Pro.local>
2016-05-13 12:22:51 +0800

    Added more validation tests and added some relation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
-describe User do
+describe User, :type => :model do
+
 	context 'validation' do
-		it { is_expected.to validate_length_of(:password).is_at_least(3)  }
 		it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_uniqueness_of :email }
 		it { is_expected.to validate_presence_of :username }
 		it { is_expected.to validate_presence_of :name }
+
+    it { is_expected.to validate_length_of(:password).is_at_least(3) }
+    it { is_expected.to validate_confirmation_of :password }
 	end
 
+  context 'relations' do
+    user = FactoryGirl.build(:user)
+    it { is_expected.to have_many :tweets }
+    it { is_expected.to have_many :likes }
+    it { is_expected.to have_many(:followers).through(:followee_follows).source(:follower) }
+    it { is_expected.to have_many(:followees).through(:follower_follows).source(:followee) }
+    it { expect{ user.destroy }.to change { Tweet.count } }
+  end
 
 end
